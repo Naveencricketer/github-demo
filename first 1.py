57e3ccc9ab3407228a371f17eb5de4fb0a06173a
naveenrakiyappan <naveenrakiyappan@gmail.com>
2024-01-31 01:25:29 +0530

    navi

--- first 1.py
+++ first 1.py
@@ -5,3 +5,10 @@ def print_hello_world():
 
 # Call the function to print the message
 print_hello_world()
+# Simple Python code to print "Hello, World!"
+
+def print_hello_():
+    print("Hello,")
+
+# Call the function to print the message
+print_hello_()
